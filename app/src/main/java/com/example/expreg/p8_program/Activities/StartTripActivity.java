0dab8ff48f1a0513d9b708ffbab5091694c1733b
Aherm <anders.hermansen1991@gmail.com>
2016-05-08 19:22:06 +0200

    Created a new activity StartTripActivity

---
+++ app/src/main/java/com/example/expreg/p8_program/Activities/StartTripActivity.java
@@ -0,0 +1,20 @@
+package com.example.expreg.p8_program.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.expreg.p8_program.R;
+
+public class StartTripActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_trip);
+    }
+
+    public void stopTrip(View view) {
+
+    }
+}

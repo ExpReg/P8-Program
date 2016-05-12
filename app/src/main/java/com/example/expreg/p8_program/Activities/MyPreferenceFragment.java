6a6c4d578121488b425724ae862369dbbf94f0d7
Aherm <anders.hermansen1991@gmail.com>
2016-05-12 11:49:24 +0200

    Fixed the detection style setting

--- app/src/main/java/com/example/expreg/p8_program/Activities/MyPreferenceFragment.java
+++ app/src/main/java/com/example/expreg/p8_program/Activities/MyPreferenceFragment.java
@@ -1,5 +1,6 @@
 package com.example.expreg.p8_program.Activities;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
@@ -8,6 +9,7 @@ import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.example.expreg.p8_program.DB.MySQLiteHelper;
@@ -24,6 +26,21 @@ public class MyPreferenceFragment extends PreferenceFragment implements SharedPr
             Preference preference = getPreferenceScreen().getPreference(i);
             updatePreference(preference);
         }
+
+        findPreference("pref_detectionStyle").setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                String val = (String) newValue;
+                SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+                if (val.equals("lenient")) {
+                    sharedPrefs.edit().putFloat("pref_accelerationThreshold", 3.0f).putFloat("pref_decelerationThreshold", 4.0f).apply();
+                }
+                else if (val.equals("strict")) {
+                    sharedPrefs.edit().putFloat("pref_accelerationThreshold", 1.5f).putFloat("pref_decelerationThreshold", 2.0f).apply();
+                }
+                return true;
+            }
+        });
 
         findPreference("pref_exportDB").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
@@ -70,15 +87,6 @@ public class MyPreferenceFragment extends PreferenceFragment implements SharedPr
 
     public void updatePreference (Preference pref) {
         if (pref == null) return;
-        if (pref.getKey().equals("pref_detectionStyle")) {
-            SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
-            if (sharedPrefs.getString("pref_detectionStyle","default").equals("lenient")) {
-                sharedPrefs.edit().putFloat("pref_accelerationThreshold", 3.0f).putFloat("pref_decelerationThreshold", 4.0f).apply();
-            }
-            else if (sharedPrefs.getString("pref_detectionStyle","default").equals("strict")) {
-                sharedPrefs.edit().putFloat("pref_accelerationThreshold", 1.5f).putFloat("pref_decelerationThreshold", 2.0f).apply();
-            }
-        }
         if (pref instanceof EditTextPreference) {
             EditTextPreference editPref = (EditTextPreference) pref;
             pref.setSummary(editPref.getText());
